2b818cef3c6674f2641a28c8074d2c7805918e27
Anurag3703 <146084286+Anurag3703@users.noreply.github.com>
2025-04-19 02:08:16 +0200

    Add

---
+++ src/main/java/org/example/deadlinescheduler/config/DatabaseConfig.java
@@ -0,0 +1,16 @@
+package org.example.deadlinescheduler.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+        return adapter;
+    }
+}
