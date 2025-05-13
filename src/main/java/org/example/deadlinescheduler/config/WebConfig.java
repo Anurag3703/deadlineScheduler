eb0b9d14bc98a0411f55b4186ac23d326e9c2a77
Anurag3703 <146084286+Anurag3703@users.noreply.github.com>
2025-05-13 03:49:59 +0200

    Cors

--- src/main/java/org/example/deadlinescheduler/config/WebConfig.java
+++ src/main/java/org/example/deadlinescheduler/config/WebConfig.java
@@ -9,16 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000",
+                .allowedOrigins(
+                        "http://localhost:3000",
                         "https://on-time-frontend.vercel.app",
                         "https://on-time-frontend-anurag3703s-projects.vercel.app",
-                        "https://on-time-frontend-d8xknc31w-anurag3703s-projects.vercel.app",
-                        "https://*.vercel.app")
+                        "https://on-time-frontend-d8xknc31w-anurag3703s-projects.vercel.app"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
-
-
 }
